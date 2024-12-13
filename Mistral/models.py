aae7ede65dc20037856ae92331ba6094f19206b8
BaptisteEtroy <betroy.ieu2022@student.ie.edu>
2024-12-13 21:02:14 +0100

    changes for multiple conversations

--- Mistral/models.py
+++ Mistral/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Text
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy.orm import declarative_base, sessionmaker
+from config import settings
 
 Base = declarative_base()
 
@@ -9,3 +10,13 @@ class Message(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     role = Column(String, index=True)  # user or chatbot
     content = Column(Text)
+
+def get_engine(db_name: str):
+    db_path = f"{settings.DATABASE_DIR}/{db_name}.db"
+    return create_engine(f"sqlite:///{db_path}", connect_args={"check_same_thread": False})
+
+def get_session(db_name: str):
+    engine = get_engine(db_name)
+    SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+    Base.metadata.create_all(bind=engine)
+    return SessionLocal()
